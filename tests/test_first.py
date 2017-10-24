09d942fb7ad0410bfd5f44d46ad83dc7afe2cd65
Patrick Höhn <hoehnp@gmx.de>
2017-10-24 23:52:06 +0200

    removed not needed commented code added functionality to open menu and first steps to download map

--- tests/test_first.py
+++ tests/test_first.py
@@ -45,36 +45,14 @@ class SimpleAndroidTests(unittest.TestCase):
             el.click()
         except Exception as e:
             pass
+        self.driver.press_keycode(82)
         try:
-            self.driver.pressMenu()
-            
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("Download")')
+            el.click()
         except Exception as e:
             pass
 
-        #el = self.driver.find_element_by_accessibility_id('Graphics')
-        #el.click()
-        #el = self.driver.find_element_by_accessibility_id('Arcs')
-        #self.assertIsNotNone(el)
-
-        self.driver.back()
-
-        #el = self.driver.find_element_by_accessibility_id("App")
-        #self.assertIsNotNone(el)
-
-        #els = self.driver.find_elements_by_android_uiautomator("new UiSelector().clickable(true)")
-        #self.assertGreaterEqual(12, len(els))
-
-        #self.driver.find_element_by_android_uiautomator('text("API Demos")')
-
-
-    #def test_simple_actions(self):
-        #el = self.driver.find_element_by_accessibility_id('Graphics')
-        #el.click()
-
-        #el = self.driver.find_element_by_accessibility_id('Arcs')
-        #el.click()
-
-        #self.driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
+        sleep(10)
 
 
 if __name__ == '__main__':
