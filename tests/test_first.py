1f1f58073f55f18b2f496c005ce54c4268b26a4a
Patrick Höhn <hoehnp@gmx.de>
2017-10-24 22:37:18 +0200

    first version trying to test to open the menu

--- tests/test_first.py
+++ tests/test_first.py
@@ -12,14 +12,7 @@ PATH = lambda p: os.path.abspath(
 
 class SimpleAndroidTests(unittest.TestCase):
     def setUp(self):
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '4.2'
-        desired_caps['deviceName'] = 'Android Emulator'
-        desired_caps['app'] = PATH(
-            '../../../sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk'
-        )
-
+        desired_caps={}
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
     def tearDown(self):
@@ -27,30 +20,61 @@ class SimpleAndroidTests(unittest.TestCase):
         self.driver.quit()
 
     def test_find_elements(self):
-        el = self.driver.find_element_by_accessibility_id('Graphics')
-        el.click()
-        el = self.driver.find_element_by_accessibility_id('Arcs')
-        self.assertIsNotNone(el)
+        try:
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("Google")')
+            el.click()
+        except Exception as e:
+            pass
+        try:
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("Always")')
+            el.click()
+        except Exception as e:
+            pass
+        try:
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("OK")')
+            el.click()
+        except Exception as e:
+            pass
+        try:
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("Yes")')
+            el.click()
+        except Exception as e:
+            pass
+        try:
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("OK")')
+            el.click()
+        except Exception as e:
+            pass
+        try:
+            self.driver.pressMenu()
+            
+        except Exception as e:
+            pass
+
+        #el = self.driver.find_element_by_accessibility_id('Graphics')
+        #el.click()
+        #el = self.driver.find_element_by_accessibility_id('Arcs')
+        #self.assertIsNotNone(el)
 
         self.driver.back()
 
-        el = self.driver.find_element_by_accessibility_id("App")
-        self.assertIsNotNone(el)
+        #el = self.driver.find_element_by_accessibility_id("App")
+        #self.assertIsNotNone(el)
 
-        els = self.driver.find_elements_by_android_uiautomator("new UiSelector().clickable(true)")
-        self.assertGreaterEqual(12, len(els))
+        #els = self.driver.find_elements_by_android_uiautomator("new UiSelector().clickable(true)")
+        #self.assertGreaterEqual(12, len(els))
 
-        self.driver.find_element_by_android_uiautomator('text("API Demos")')
+        #self.driver.find_element_by_android_uiautomator('text("API Demos")')
 
 
-    def test_simple_actions(self):
-        el = self.driver.find_element_by_accessibility_id('Graphics')
-        el.click()
+    #def test_simple_actions(self):
+        #el = self.driver.find_element_by_accessibility_id('Graphics')
+        #el.click()
 
-        el = self.driver.find_element_by_accessibility_id('Arcs')
-        el.click()
+        #el = self.driver.find_element_by_accessibility_id('Arcs')
+        #el.click()
 
-        self.driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
+        #self.driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
 
 
 if __name__ == '__main__':
