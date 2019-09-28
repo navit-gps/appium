b21d99e8336cd473f96251f277ba59aad0a55ce9
Pierre Grandin <grandinp@gmail.com>
2019-09-27 18:39:40 -0600

    Some tweaks for appium/stf and multiple devices

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,7 +1,10 @@
 import os
+import base64
 from time import sleep
+import sys
 
 import unittest
+import HtmlTestRunner
 
 from appium import webdriver
 
@@ -12,8 +15,26 @@ PATH = lambda p: os.path.abspath(
 
 class SimpleAndroidTests(unittest.TestCase):
     def setUp(self):
-        desired_caps={}
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        print(f"Test device : {sys.argv[1]}")
+        desired_caps = {
+            'automationName': 'UiAutomator2',
+            'platformName': 'Android',
+            'deviceName': 'dummy',
+            'udid': sys.argv[2],
+            'autoGrantPermissions': True,
+            'app': 'https://17438-30791823-gh.circle-artifacts.com/0/apk/debug/android-debug.apk',
+        }
+        self.driver = webdriver.Remote('http://192.168.1.207:4723/wd/hub', desired_caps)
+        self.driver.save_screenshot(f"results/{sys.argv[1]}-startup.png")
+
+        with open("../colorado.bin", "rb") as f:
+            data = base64.b64encode(f.read()).decode('UTF-8')
+            self.driver.push_file('/sdcard/navit/colorado.bin', data)
+
+        # data = open("../colorado.xml", "rb").read()
+        # self.driver.push_file('/sdcard/navit/colorado.xml', base64.b64encode(data))
+        # data = open("../colorado.bin", "rb").read()
+        # self.driver.push_file('/sdcard/navit/colorado.bin', base64.b64encode(data))
 
     def tearDown(self):
         # end the session
@@ -40,6 +61,8 @@ class SimpleAndroidTests(unittest.TestCase):
             el.click()
         except Exception as e:
             pass
+
+        self.driver.save_screenshot(f"results/{sys.argv[1]}-home.png")
         try:
             el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("OK")')
             el.click()
@@ -51,10 +74,23 @@ class SimpleAndroidTests(unittest.TestCase):
             el.click()
         except Exception as e:
             pass
+        try:
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("Maps containing")')
+            el.click()
+        except Exception as e:
+            pass
+        try:
+            el = self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("Colorado")')
+            el.click()
+        except Exception as e:
+            pass
+        self.driver.save_screenshot(f"results/{sys.argv[1]}-download.png")
 
-        sleep(10)
+        print("Pausing 30s")
+        sleep(30)
 
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SimpleAndroidTests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    testRunner = HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_name=sys.argv[1], output="results")
+    unittest.main(testRunner=testRunner, argv=sys.argv[2:])
